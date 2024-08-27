426ab60096af8c5661056a31dd7167b62406126d
helloJosh <kimjoshua135@gmail.com>
2024-08-28 00:10:56 +0900

    first commit

---
+++ src/main/java/com/lilac/soaback/SoaBackApplication.java
@@ -0,0 +1,13 @@
+package com.lilac.soaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoaBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoaBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lilac/soaback/SoaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lilac.soaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoaBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
